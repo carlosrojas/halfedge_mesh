8b8facf3e0509fb9fe985ab2aeaecb01d215924e
Carlos Rojas <crojas@ucdavis.edu>
2014-10-09 10:23:18 -0700

    Add setup at root
    Has basic information, e.g. version, contact info, etc.
    
    TODO:
       * Add tests to allow python setup.py test to work correctly
       * Add download URL
       * Figure out how to add package.__version__

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='halfedge_mesh',
+    author_email="crojas@ucdavis.edu",
+    description="Halfedge data structure for triangulated meshes",
+    version='1.0',
+    packages=['halfedge_mesh']
+)
+
