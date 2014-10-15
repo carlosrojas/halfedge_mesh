006acf35dc747a8b42ca81ff14b5873632733d64
Carlos Rojas <crojas@ucdavis.edu>
2014-10-14 21:47:09 -0700

    -(untested) Add test; Finished algorithm
    The complete algorithm was implement but it has not been tested.
    
    TODO:
        * Test all py.test
        * All new unit test to cover new functions
        * Code review
        * Future: add another mesh to test

--- tests/halfedge_mesh_test.py
+++ tests/halfedge_mesh_test.py
@@ -15,18 +15,25 @@ class TestHalfedgeMesh:
         halfedges = cube_off_mesh.facets[0].halfedges()
 
         # Edge vertex 0 -> 1
-        assert halfedges[0].vertex() == halfedge_mesh.Vertex(1, -1, 1, 0)
-        assert halfedges[0].prev().vertex() == halfedge_mesh.Vertex(1, -1, -1,
-                                                                    0)
+        assert halfedges[0].vertex == halfedge_mesh.Vertex(1, -1, 1, 0)
+        assert halfedges[0].prev.vertex == halfedge_mesh.Vertex(1, -1, -1,
+                                                                0)
 
         # Edge vertex 1 -> 2
-        assert halfedges[1].vertex() == halfedge_mesh.Vertex(-1, -1, 1, 0)
-        assert halfedges[1].prev().vertex() == halfedge_mesh.Vertex(1, -1, 1, 0)
+        assert halfedges[1].vertex == halfedge_mesh.Vertex(-1, -1, 1, 0)
+        assert halfedges[1].prev.vertex == halfedge_mesh.Vertex(1, -1, 1, 0)
 
         # Edge vertex 2 -> 0
-        assert halfedges[2].vertex() == halfedge_mesh.Vertex(-1, -1, 1, 0)
-        assert halfedges[2].prev().vertex() == halfedge_mesh.Vertex(1, -1, -1,
-                                                                    0)
+        assert halfedges[2].vertex == halfedge_mesh.Vertex(-1, -1, 1, 0)
+        assert halfedges[2].prev.vertex == halfedge_mesh.Vertex(1, -1, -1, 0)
+
+    def test_facet_eq_correct_for_same_object_and_diff_objects(self,
+                                                               cube_off_mesh):
+        assert cube_off_mesh.facets[0] == cube_off_mesh.facets[0]
+        assert cube_off_mesh.facets[1] != cube_off_mesh.facets[0]
+
+        assert cube_off_mesh.facets[3] == cube_off_mesh.facets[3]
+        assert cube_off_mesh.facets[0] != cube_off_mesh.facets[3]
 
     def test_halfedgemesh_vertices_are_in_order_with_cubeoff(self,
                                                              cube_off_mesh):
@@ -63,28 +70,38 @@ class TestHalfedgeMesh:
             self, cube_off_mesh):
 
         five_seven = cube_off_mesh.get_halfedge(5, 7)
-        assert five_seven.vertex().index == 7
-        assert five_seven.prev().vertex().index == 5
+        assert five_seven.vertex.index == 7
+        assert five_seven.prev.vertex.index == 5
 
         five_six = cube_off_mesh.get_halfedge(5, 6)
-        assert five_six.vertex().index == 6
-        assert five_six.prev().vertex().index == 5
+        assert five_six.vertex.index == 6
+        assert five_six.prev.vertex.index == 5
 
         one_two = cube_off_mesh.get_halfedge(1, 2)
-        assert one_two.vertex().index == 1
-        assert one_two.prev().index == 2
+        assert one_two.vertex.index == 1
+        assert one_two.prev.index == 2
 
     def test_halfedge_opposite_correct_vertices_with_cubeoff(self,
                                                              cube_off_mesh):
 
         zero_two = cube_off_mesh.get_halfedge(0, 2)
-        assert zero_two.opposite().vertex().index == 0
-        assert zero_two.opposite().prev().vertex().index == 2
+        assert zero_two.opposite.vertex.index == 0
+        assert zero_two.opposite.prev.vertex.index == 2
 
         zero_one = cube_off_mesh.get_halfedge(0, 1)
-        assert zero_one.opposite().vertex().index == 0
-        assert zero_one.opposite().prev().vertex().index == 1
+        assert zero_one.opposite.vertex.index == 0
+        assert zero_one.opposite.prev.vertex.index == 1
 
         four_one = cube_off_mesh.get_halfedge(4, 1)
-        assert four_one.opposite().vertex().index == 4
-        assert four_one.opposite().prev().vertex().index == 1
+        assert four_one.opposite.vertex.index == 4
+        assert four_one.opposite.prev.vertex.index == 1
+
+    def test_halfedge_eq_correct_for_same_and_object_and_diff_objects(self,
+                                                              cube_off_mesh):
+
+        zero_two = cube_off_mesh.get_halfedge(0, 2)
+        assert zero_two == zero_two
+
+        four_one = cube_off_mesh.get_halfedge(4, 1)
+        assert zero_two != four_one
+
