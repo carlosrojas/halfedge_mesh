239b44582fd342e11597fca7b1852b9beb6e8f83
Carlos Rojas <crojas@ucdavis.edu>
2015-02-04 11:41:52 -0800

    Add new functions norm, dot, etc to __init__
    Now the functions will be accessible to test suite

--- halfedge_mesh/__init__.py
+++ halfedge_mesh/__init__.py
@@ -2,3 +2,9 @@ from halfedge_mesh import HalfedgeMesh
 from halfedge_mesh import Vertex
 from halfedge_mesh import Halfedge
 from halfedge_mesh import Facet
+from halfedge_mesh import norm
+from halfedge_mesh import normalize
+from halfedge_mesh import dot
+from halfedge_mesh import cross_product
+from halfedge_mesh import make_iterable
+from halfedge_mesh import allclose
